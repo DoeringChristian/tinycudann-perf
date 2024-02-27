5279e2911514b472663c1e97f72e147b370476e0
Christian Döring <christian.doering@tum.de>
2024-02-27 15:44:03 +0100

    Improved plot

--- plot.py
+++ plot.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
     with open("out/tcnn.pkl", "rb") as f:
         tcnn = pickle.load(f)
 
+    plt.title("Fully Fused MLP Inference")
     plt.plot(hep["batch_sizes"], hep["throughputs"], label="hephaestus")
-    plt.plot(tcnn["batch_sizes"], tcnn["throughputs"], label="tiny cuda nn")
+    plt.plot(tcnn["batch_sizes"], tcnn["throughputs"], label="tiny-cuda-nn")
     plt.xscale("log", base=2)
     plt.xlabel("batch size")
     plt.ylabel("throughput")
     plt.legend()
     # plt.savefig("out/cmp.svg")
+    plt.gcf().savefig("out/plot.svg")
     plt.show()
